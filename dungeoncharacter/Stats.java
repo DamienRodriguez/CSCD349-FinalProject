e1b12617585b70e166b1e670d56acb2415e4a2bb
Damien <drodriguez17@eagles.ewu.edu>
2020-03-05 13:55:35 -0800

    initial commit with Stats class complete

---
+++ dungeoncharacter/Stats.java
@@ -0,0 +1,76 @@
+public class Stats {
+
+	private int healthPoints;
+	private int attackSpeed;
+	private double chanceToHit;
+	private int minDamage, maxDamage;
+	private double chanceToBlock;
+	private double chanceToHeal;
+	private int minHeal, maxHeal;
+	
+	public Stats(final int healthPoints, final int attackSpeed, final double chanceToHit, final int minDamage, final int maxDamage, final double chanceToBlock) { 
+		setHP(healthPoints);
+		setAS(attackSpeed);
+		setChanceToHit(chanceToHit);
+		setDamageVariance(minDamage,maxDamage);
+		setChanceToBlock(chanceToBlock);
+	} //end of Hero Stats constructor
+	
+	public Stats(final int healthPoints, final int attackSpeed, final double chanceToHit, final int minDamage, final int maxDamage, final double chanceToHeal, final int minHeal, final int maxHeal) {
+		setHP(healthPoints);
+		setAS(attackSpeed);
+		setChanceToHit(chanceToHit);
+		setDamageVariance(minDamage,maxDamage);
+		setChanceToHeal(chanceToHeal);
+		setHealVariance(minHeal,maxHeal);
+	}  //end of Monster Stats constructor
+	
+	public void setHP(final int healthPoints) { 
+		this.healthPoints = healthPoints; 
+	}
+	public void setAS(final int attackSpeed) { 
+		this.attackSpeed = attackSpeed; 
+	}
+	public void setChanceToHit(final double chanceToHit) { 
+		this.chanceToHit = chanceToHit; 
+	}
+	public void setDamageVariance(final int minDamage, final int maxDamage) { 
+		this.minDamage = minDamage; 
+		this.maxDamage = maxDamage; 
+	}
+	public void setHealVariance(final int minHeal, final int maxHeal) { 
+		this.minHeal = minHeal; 
+		this.maxHeal = maxHeal; 
+	}
+	public void setChanceToHeal(final double chanceToHeal) { 
+		this.chanceToHeal = chanceToHeal; 
+	}
+	public void setChanceToBlock(final double chanceToBlock) { 
+		this.chanceToBlock = chanceToBlock; 
+	}
+	
+	public int getHP() { 
+		return this.healthPoints; 
+	}
+	public int getAS() { 
+		return this.attackSpeed; 
+	}
+	public double getChanceToHit() { 
+		return this.chanceToHit; 
+	}
+	
+	public int[] getDamageVariance() {
+      int[] var = {this.minDamage, this.maxDamage};
+		return var;  
+	}
+	public int[] getHealVariance() {
+      int[] var = {this.minHeal, this.maxHeal};
+		return var;  
+   }
+	public double getChanceToHeal() { 
+		return this.chanceToHeal; 
+	}
+	public double getChanceToBlock() { 
+		return this.chanceToBlock; 
+	}
+}
