d16524ce6a9bffee028fe15aed28d3dbcc19fa59
Kevin Underwood <kunderwood1@eagles.ewu.edu>
2020-03-16 14:24:03 -0700

    Delete Attack.java

--- dungeon/dungeoncharacter/Attack.java
+++
@@ -1,6 +0,0 @@
-package dungeoncharacter;
-public interface Attack {
-	public abstract void attack(final DungeonCharacter opponent);
-	public abstract void addHP(final int hp);
-	public abstract void subHP(final int hp);
-}
